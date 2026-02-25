97817638d1187145296ac53ed9d7a87046559500
Arttu Åkerblom <arttuake@gmail.com>
2026-02-25 19:56:07 +0100

    Remove comments from main

--- src/main.cpp
+++ src/main.cpp
@@ -19,29 +19,29 @@ int main(int ac, char **av) {
 	}
 	std::signal(SIGINT, &handle_signals);
 	
-	ParseConfig pc((std::string(av[1])));
+//	ParseConfig pc((std::string(av[1])));
 
-	// std::vector<std::string> temp;
-	// temp.push_back("8080");
-	// try {
-	// 	ServerManager serverManager(temp);
-	// 	serverManager.mainLoop();
-	// }
-	// catch (Tools::Exception& e){
-	// 	if (e.getReturnCode() == 0)
-	// 		std::clog << "Custom exception: " << PINK << e.getMsgLog() << RESET << std::endl;
-	// 	else if (e.getReturnCode() == 1)
-	// 		std::clog << ORANGE << e.getMsgLog() << RESET << std::endl;
-	// 	else
-	// 		std::clog << ORANGE << e.getMsgLog() << RESET << std::endl;
-	// }
-	// catch (std::exception &e)
-	// {
-	// 	std::clog << "Builtin exception: " << ORANGE << e.what() << RESET << std::endl;
-	// }
-	// catch (...)
-	// {
-	// 	std::clog << ORANGE << "Undefined error" << RESET << std::endl;
-	// }
+	std::vector<std::string> temp;
+	temp.push_back("8080");
+	try {
+		ServerManager serverManager(temp);
+		serverManager.mainLoop();
+	}
+	catch (Tools::Exception& e){
+		if (e.getReturnCode() == 0)
+			std::clog << "Custom exception: " << PINK << e.getMsgLog() << RESET << std::endl;
+		else if (e.getReturnCode() == 1)
+			std::clog << ORANGE << e.getMsgLog() << RESET << std::endl;
+		else
+			std::clog << ORANGE << e.getMsgLog() << RESET << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::clog << "Builtin exception: " << ORANGE << e.what() << RESET << std::endl;
+	}
+	catch (...)
+	{
+		std::clog << ORANGE << "Undefined error" << RESET << std::endl;
+	}
 	return 0;
 }
