894e05461bde11ae11b23db812632ac657e7d2ef
Arttu Åkerblom <arttuake@gmail.com>
2026-02-25 20:15:32 +0100

    Move parsing classes to parsing folder

--- src/parsing/ServerBlockConfig.cpp
+++ src/parsing/ServerBlockConfig.cpp
@@ -1,4 +1,4 @@
-#include "ServerBlockConfig.hpp"
+#include "parsing/ServerBlockConfig.hpp"
 
 /*
 =================================================================
@@ -13,7 +13,8 @@ ServerBlockConfig::ServerBlockConfig(const ServerBlockConfig &obj) { *this = obj
 
 ServerBlockConfig::ServerBlockConfig(std::ifstream& infile, bool startingBraceIncluded) {
 	std::cout << "Hello from inside the ServerBlockConfig" << std::endl;
-
+	(void)infile;
+	(void)startingBraceIncluded;
 }
 
 /*
