d1ed5cbfd9930fdade611496fec3edec0a2a4d93
Arttu Åkerblom <arttuake@gmail.com>
2026-02-27 11:57:05 +0100

    Create logic for directive name handlers

--- include/parsing/ServerBlockConfig.hpp
+++ include/parsing/ServerBlockConfig.hpp
@@ -12,11 +12,23 @@ class ServerBlockConfig : public ConfigBase
 {
 private:
 	std::string _port;
+	__uint8_t _duplicates;
 	std::set<std::string> _serverNames;
 	std::map<std::string, LocationConfig> _locationConfigs; // map<path, LocationConfig>
 	std::ifstream& _infile;
 	ServerBlockConfig();
 //	ServerBlockConfig(const ServerBlockConfig &obj);
+
+	typedef	bool (ServerBlockConfig::*DirectiveHandler)(const std::vector<std::string>&);
+
+	static const std::map<std::string, DirectiveHandler> _handlers;
+	static const std::map<std::string, DirectiveHandler> _initHandlers();
+
+	enum Flags {
+		NONE = 0x000,
+		LISTEN = 0x001,
+		RETURN = 0x002
+	};
 	
 public:
 	ServerBlockConfig(std::ifstream& infile, bool startingBraceIncluded);
@@ -24,4 +36,7 @@ public:
 	~ServerBlockConfig();
 
 	bool handleStartingBrace(bool startingBraceIncluded);
+	void handleDirectiveName(const std::string& line);
+	bool handleListen(const std::vector<std::string>& tokens);
+	bool handleServerName(const std::vector<std::string>& tokens);
 };
--- src/parsing/ServerBlockConfig.cpp
+++ src/parsing/ServerBlockConfig.cpp
@@ -11,13 +11,20 @@ ServerBlockConfig::~ServerBlockConfig() {}
 
 // ServerBlockConfig::ServerBlockConfig(const ServerBlockConfig &obj) { *this = obj; }
 
+// Exception on parsing error
 ServerBlockConfig::ServerBlockConfig(std::ifstream& infile, bool startingBraceIncluded) :
-	_infile(infile) 
+	_duplicates(NONE),
+	_infile(infile)
 	{
 	std::cout << "Hello from inside the ServerBlockConfig" << std::endl;
 	std::string line;
+
 	if (!handleStartingBrace(startingBraceIncluded)) {
 		throw Tools::Exception("Parsing error");
+	}
+	while (std::getline(_infile, line)) {
+		std::vector<std::string> tokens(Tools::splitString(line));
+		handleDirectiveName(line);
 	}
 }
 
@@ -42,6 +49,34 @@ ServerBlockConfig &ServerBlockConfig::operator=(const ServerBlockConfig &obj)
 
 /*
 =================================================================
+===== DIRECTIVE HANDLERS ========================================
+=================================================================
+*/
+
+const std::map<std::string, ServerBlockConfig::DirectiveHandler> ServerBlockConfig::_handlers
+	= ServerBlockConfig::_initHandlers();
+
+const std::map<std::string, ServerBlockConfig::DirectiveHandler> ServerBlockConfig::_initHandlers() {
+	std::map<std::string, ServerBlockConfig::DirectiveHandler> temp;
+
+	temp.insert(std::make_pair("server", &ServerBlockConfig::handleListen));
+	temp.insert(std::make_pair("server_name", &ServerBlockConfig::handleServerName));
+
+	return temp;
+}
+
+bool ServerBlockConfig::handleListen(const std::vector<std::string>& tokens) {
+	(void)tokens;
+	return true;
+}
+
+bool ServerBlockConfig::handleServerName(const std::vector<std::string>& tokens) {	
+	(void)tokens;
+	return true;
+}
+
+/*
+=================================================================
 ===== METHODS ===================================================
 =================================================================
 */
@@ -56,3 +91,19 @@ bool ServerBlockConfig::handleStartingBrace(bool startingBraceIncluded) {
 	}
 	return true;
 }
+
+void ServerBlockConfig::handleDirectiveName(const std::string& line) {
+	std::vector<std::string> tokens(Tools::splitString(line));
+	if (tokens.size() < 2)
+		Tools::Exception("Parsing error");
+
+	std::map<std::string, ServerBlockConfig::DirectiveHandler>::const_iterator it;
+	it = _handlers.find(tokens[0]);
+	if (it == _handlers.end())
+		Tools::Exception("Unknown directive name");
+	
+	DirectiveHandler handler = it->second;
+	if (!(this->*handler)(tokens))
+		Tools::Exception(tokens[0] + " directive not valid");
+}
+
