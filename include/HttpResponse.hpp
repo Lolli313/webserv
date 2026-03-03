66677aea664bb52a716194893693fe94b9cb5d16
Thomas.42 <0xscarats@gmail.com>
2026-03-03 19:48:33 +0100

    see previous commit.

--- include/HttpResponse.hpp
+++ include/HttpResponse.hpp
@@ -52,7 +52,7 @@ const std::string quickHttpReponse(int code, const std::string &message);
 
 /**
  * @brief Get an HTTP Response formatted without having to create an object. Directly with a pair<code, message>
- * @param  pair<return code, message> : A ReturnPair (can be found in HttpTools{.hpp/.cpp}) of int (return code) and const std::string & (Reason Phrase / return message).
+ * @param reponse pair<return code, message> : A ReturnPair (can be found in HttpTools{.hpp/.cpp}) of int (return code) and const std::string & (Reason Phrase / return message).
  * @return A const std::string formatted and ready to be sent to the client. With the default HTTP_VERSION, the return code and the return message.
  */
 const std::string quickHttpReponse(std::pair<int, const std::string &> &response);
