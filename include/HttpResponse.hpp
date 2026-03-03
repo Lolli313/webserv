b61f2cbe746e1b6df8facb05bcfae00ff451fc70
Thomas.42 <0xscarats@gmail.com>
2026-03-03 19:42:31 +0100

    HttpResponse data structure and logic started.
    Some useful functions to enhance the usage experience.
    
    You will be able to create a response in details, or quickly with the quickHttpResponses, that will be able to return a formated HTTP Response directly from a int code + std::string message, or directly with the pair<code, message>.

--- include/HttpResponse.hpp
+++ include/HttpResponse.hpp
@@ -3,13 +3,17 @@
 #include <string>
 #include <map>
 
+#define HTTP_VERSION "HTTP/1.1"
+
+/**
+ * @brief A HTTP Response class.
+ * @details
+ */
 class HttpResponse
 {
 private:
 	// HTTP VERSION
 	std::string _httpVersion;
-	int _httpVersionMinor;
-	int _httpVersionMajor;
 
 	// RETURN CODE & MESSAGE
 	int _returnCode;
@@ -24,10 +28,31 @@ private:
 
 	// FINAL RESPONSE TO BE STRANSFERED TO THE CLIENT
 	std::string _finalResponse;
+	HttpResponse();
 
 public:
-	HttpResponse();
+	HttpResponse(int code, const std::string &message);
+	HttpResponse(const std::string &httpVersion, int code, const std::string &message);
 	HttpResponse(const HttpResponse &obj);
 	HttpResponse &operator=(const HttpResponse &obj);
 	~HttpResponse();
+
+	void setResponseHeaders(std::map<std::string, std::string> responseHeaders);
+	void setRepresentationHeaders(std::map<std::string, std::string> representationHeaders);
+
+	const std::string &getFinalResponse() const; // This call builds and return the final response.
 };
+
+/**
+ * @brief Get an HTTP Response formatted without having to create an object.
+ * @param The return code and the Reason-Phrase (return message).
+ * @return A const std::string formatted and ready to be sent to the client. With the default HTTP_VERSION, the return code and the return message.
+ */
+const std::string quickHttpReponse(int code, const std::string &message);
+
+/**
+ * @brief Get an HTTP Response formatted without having to create an object. Directly with a pair<code, message>
+ * @param  pair<return code, message> : A ReturnPair (can be found in HttpTools{.hpp/.cpp}) of int (return code) and const std::string & (Reason Phrase / return message).
+ * @return A const std::string formatted and ready to be sent to the client. With the default HTTP_VERSION, the return code and the return message.
+ */
+const std::string quickHttpReponse(std::pair<int, const std::string &> &response);
