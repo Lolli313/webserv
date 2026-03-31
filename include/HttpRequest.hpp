5a8b8bc9f0642d2a1acb13023af8990df5c80c77
Thomas.42 <0xscarats@gmail.com>
2026-03-31 17:11:11 +0200

    percent encoding is being handled.

--- include/HttpRequest.hpp
+++ include/HttpRequest.hpp
@@ -20,47 +20,48 @@
 #include <cstdlib>
 #include <unistd.h>
 
+
 class Post;
 
-class HttpRequest {
+class HttpRequest
+{
 
-  private:
+private:
+  std::string _methodStr;
+  std::string _path;
+  std::string _purePath;
+  std::map<std::string, std::string> _queryParams;
+  std::string _httpVersion;
+  std::map<std::string, std::string> _header;
+  std::string _boundary;
+  std::string _body;
 
-    std::string _methodStr;
-    std::string _path;
-    std::string _purePath;
-    std::map<std::string, std::string> _queryParams;
-    std::string _httpVersion;
-    std::map<std::string, std::string> _header;
-    std::string _boundary;
-    std::string _body;
+public:
+  // CONSTRUCTORS
 
-  public:
+  HttpRequest();
+  HttpRequest(const HttpRequest &other);
+  HttpRequest &operator=(const HttpRequest &other);
+  ~HttpRequest();
 
-    // CONSTRUCTORS
+  // GETTERS
 
-    HttpRequest();
-    HttpRequest(const HttpRequest &other);
-    HttpRequest &operator=(const HttpRequest &other);
-    ~HttpRequest();
+  const std::string &getMethodStr() const { return _methodStr; }
+  const std::string &getPath() const { return _path; }
+  const std::string &getPurePath() const { return _purePath; }
+  const std::map<std::string, std::string> &getQueryParams() const { return _queryParams; }
+  const std::string &getHttpVersion() const { return _httpVersion; }
+  const std::map<std::string, std::string> &getHeader() const { return _header; }
+  const std::string &getBoundary() const { return _boundary; }
+  const std::string &getBody() const { return _body; }
 
-    // GETTERS
+  // FUNCTIONS
 
-    const std::string &getMethodStr() const { return _methodStr;}
-    const std::string &getPath() const {return _path;}
-    const std::string &getPurePath() const {return _purePath;}
-    const std::map<std::string, std::string> &getQueryParams() const {return _queryParams;}
-    const std::string &getHttpVersion() const {return _httpVersion;}
-    const std::map<std::string, std::string> &getHeader() const {return _header;}
-    const std::string &getBoundary() const {return _boundary;}
-    const std::string &getBody() const {return _body;}
-
-    // FUNCTIONS
-
-    void parseQueryParams();
-    void parse(const std::string &request);
-    void cookie(Cookie &cookie);
-    void executeScript();
-    // void executeResponse();
-    void print() const;
+  void cleanPath();
+  void parseQueryParams();
+  void parse(const std::string &request);
+  void cookie(Cookie &cookie);
+  void executeScript();
+  // void executeResponse();
+  void print() const;
 };
